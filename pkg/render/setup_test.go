d1c6206439cbe62d03b96271ea87d7d4b7872032
Adam Boudreau <adam.boudreau@rci.rogers.com>
2023-04-04 17:30:03 -0400

    add renderer test

---
+++ pkg/render/setup_test.go
@@ -0,0 +1,33 @@
+package render
+
+import (
+	"bookings/pkg/config"
+	"bookings/pkg/models"
+	"encoding/gob"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+	gob.Register(models.Reservation{}) // allow storing reservation model in session
+
+	testApp.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = testApp.InProduction
+	testApp.Session = session
+
+	app = &testApp
+
+	os.Exit(m.Run())
+}
