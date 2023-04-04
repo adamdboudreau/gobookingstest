b8626451ecf63beb21b5f15167ace2096c9470ce
Adam Boudreau <adam.boudreau@rci.rogers.com>
2023-04-04 16:51:33 -0400

    add handler tests

---
+++ pkg/handlers/setup_test.go
@@ -0,0 +1,130 @@
+package handlers
+
+import (
+	"bookings/pkg/config"
+	"bookings/pkg/models"
+	"bookings/pkg/render"
+	"encoding/gob"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	// "github.com/go-chi/chi"
+	// "github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/justinas/nosurf"
+)
+
+var app config.AppConfig
+var session *scs.SessionManager
+var pathToTemplates = "./../../templates"
+var functions = template.FuncMap{}
+
+func getRoutes() http.Handler {
+	gob.Register(models.Reservation{}) // allow storing reservation model in session
+
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+	app.Session = session
+
+	tc, err := CreateTestTemplateCache()
+	if err != nil {
+		log.Fatal("cannot load templates")
+		// return err
+	}
+	app.TemplateCache = tc
+	app.UseCache = true
+
+	repo := NewRepo(&app)
+	NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	// mux.Use(WriteToConsole)
+	// mux.Use(NoSurf) // avoid checking csrf-token
+	mux.Use(SessionLoad)
+
+	mux.Get("/", Repo.Home)
+	mux.Get("/about", Repo.About)
+	mux.Get("/generals", Repo.Generals)
+	mux.Get("/majors", Repo.Majors)
+	mux.Get("/search_availability", Repo.SearchAvailability)
+	mux.Post("/search_availability", Repo.PostAvailability)
+	mux.Post("/availability_json", Repo.AvailabilityJSON)
+	mux.Get("/book_room", Repo.BookRoom)
+	mux.Post("/book_room", Repo.PostBookRoom)
+	mux.Get("/reservation_summary", Repo.ReservationSummary)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
+
+// NoSurf adds csrf token to each request
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   app.InProduction,
+		SameSite: http.SameSiteLaxMode,
+	})
+
+	return csrfHandler
+}
+
+// SessionLoad loads & saves session on every request
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
+}
+
+func CreateTestTemplateCache() (map[string]*template.Template, error) {
+	//myCache := make(map[string]*template.Template)
+	myCache := map[string]*template.Template{}
+
+	// get all files named *.page.tmpl from ./templates
+	// pages, err := filepath.Glob("./templates/*.page.tmpl")
+	pages, err := filepath.Glob(fmt.Sprintf("%s/*.page.tmpl", pathToTemplates))
+	if err != nil {
+		return myCache, err
+	}
+
+	// range through all files with page.tmpl
+	for _, page := range pages {
+		name := filepath.Base(page) // file.page.tmpl
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+		matches, err := filepath.Glob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplates))
+		if err != nil {
+			return myCache, err
+		}
+		if len(matches) > 0 {
+			// add any templates to the parsed page
+			ts, err = ts.ParseGlob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplates))
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+}
--- pkg/render/renderer.go
+++ pkg/render/renderer.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -14,6 +15,8 @@ import (
 )
 
 var app *config.AppConfig
+var pathToTemplates = "./templates"
+var functions = template.FuncMap{}
 
 // NewTemplates sets config for template
 func NewTemplates(a *config.AppConfig) {
@@ -74,7 +77,8 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 	myCache := map[string]*template.Template{}
 
 	// get all files named *.page.tmpl from ./templates
-	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	// pages, err := filepath.Glob("./templates/*.page.tmpl")
+	pages, err := filepath.Glob(fmt.Sprintf("%s/*.page.tmpl", pathToTemplates))
 	if err != nil {
 		return myCache, err
 	}
@@ -82,14 +86,17 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 	// range through all files with page.tmpl
 	for _, page := range pages {
 		name := filepath.Base(page) // file.page.tmpl
-		ts, err := template.New(name).ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return myCache, err
 		}
-		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		matches, err := filepath.Glob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplates))
+		if err != nil {
+			return myCache, err
+		}
 		if len(matches) > 0 {
 			// add any templates to the parsed page
-			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			ts, err = ts.ParseGlob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplates))
 			if err != nil {
 				return myCache, err
 			}
